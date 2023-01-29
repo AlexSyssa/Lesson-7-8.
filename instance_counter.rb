92274fe153803c8fd880e86f745ad10b0489bf58
Bugaga <114928678+AlexSyssa@users.noreply.github.com>
2023-01-29 21:04:22 +0300

    Update instance_counter.rb

--- instance_counter.rb
+++ instance_counter.rb
@@ -1,28 +1,19 @@
 module InstanceCounter
-
 	def self.included(base)
 		base.extend ClassMethods
 		base.send :include, InstanceMethods
 	end
-
 	module ClassMethods
 		attr_accessor :instances
-		
 		def instance
 			@instances ||=0
 		end
 	end
-  
   protected
-  
-	module InstanceMethods
+  module InstanceMethods
 		def register_instance
 			self.class.instance
 			self.class.instances += 1
 		end
 	end
 end
-
-
-
-
