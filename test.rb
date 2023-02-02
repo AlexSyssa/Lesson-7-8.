cb28a2ac28ee86584b960f01d7368593bbac4c75
Bugaga <114928678+AlexSyssa@users.noreply.github.com>
2023-02-02 19:44:11 +0300

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,21 +1,21 @@
 train_cargo1 = CargoTrain.new(248, 'Cargo')
-train_passenger1 = PassengerTrain.new(250, 'Passenger')
-train_1 = Train.new(222, 'Cargo')
-train_3 = Train.new(111, 'Passenger')
+train_passenger1 = PassengerTrain.new(250, 'Passenger', 'KPM' )
+train_1 = Train.new(222, 'Cargo', 'KPM')
+train_3 = Train.new(111, 'Passenger', 'krock')
 station1 = Station.new("Удмурская")
 station2 = Station.new("Вахская")
 station3 = Station.new("Пионерская")
 station4 = Station.new("Советская")
 route1 = Route.new(station1, station2)
 route2 = Route.new(station3, station4)
-cargo_wagon1 = CargoWagon.new(11)
-cargo_wagon2 = CargoWagon.new(33)
-cargo_wagon3 = CargoWagon.new(55)
-cargo_wagon4 = CargoWagon.new(77)
-passenger_wagon1 = PassengerWagon.new(22)
-passenger_wagon2 = PassengerWagon.new(44)
-passenger_wagon3 = PassengerWagon.new(66)
-passenger_wagon4 = PassengerWagon.new(88)
+cargo_wagon1 = CargoWagon.new(11, 'Cargo', 'Gol')
+cargo_wagon2 = CargoWagon.new(33, 'Cargo', 'Gol')
+cargo_wagon3 = CargoWagon.new(55, 'Cargo', 'Gol' )
+cargo_wagon4 = CargoWagon.new(77, 'Cargo', 'Gol' )
+passenger_wagon1 = PassengerWagon.new(22, 'Passenger', 'Gong')
+passenger_wagon2 = PassengerWagon.new(44, 'Passenger', 'Gong')
+passenger_wagon3 = PassengerWagon.new(66, 'Passenger', 'Gong')
+passenger_wagon4 = PassengerWagon.new(88, 'Passenger', 'Gong')
 
 route1.add_station(station4)
 route1.add_station(station3)
@@ -42,11 +42,3 @@ station1.output_trains
 Route.instances
 Station.instances
 Train.instances
-
-
-
-
-
-
-
-
